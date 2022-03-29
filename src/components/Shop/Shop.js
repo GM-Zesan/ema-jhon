772eb905531631eb4ee2264588c2fe4aac24fe94
Zesan <gmzesan7767@gmail.com>
2022-03-29 19:05:00 +0600

    product ready

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css';
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch("products.json")
+            .then((res) => res.json())
+            .then((data) => setProducts(data));
+    },[])
     return (
         <div className="shop-container">
-            <div className="product-container">product will be appear here</div>
+            <div className="product-container">
+                {
+                    products.map(product=><Product
+                    key={product.id}
+                    product={product}
+                    ></Product>)
+                }
+            </div>
             <div className="cart-container">Cart will be shown here</div>
         </div>
     );
